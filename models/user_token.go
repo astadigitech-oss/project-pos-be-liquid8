51fe3320668bb5445e7179a9ac2778eda59bdbe3
windisiswandi <windisiswandi89@gmail.com>
2026-04-23 15:50:27 +0800

    fix: remove unique type in token

--- models/user_token.go
+++ models/user_token.go
@@ -5,7 +5,7 @@ import "time"
 type UserToken struct {
 	ID         uint      `gorm:"primaryKey;autoIncrement"`
 	UserID     uint      `gorm:"not null"`
-	Token      string    `gorm:"type:text;not null;unique"`
+	Token      string    `gorm:"type:text;not null"`
 	UserAgent  string    `gorm:"size:255"`
 	LastUsedAt time.Time `gorm:"not null"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"` // auto input
